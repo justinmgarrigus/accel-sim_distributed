17d17af4efea6a3c49dc60816ac1c4c0bf7c8a4e
justinmgarrigus <justin.m.garrigus@gmail.com>
2024-09-13 14:48:08 +0000

    Removed new install of nvbit

--- util/tracer_nvbit/install_nvbit.sh
+++ util/tracer_nvbit/install_nvbit.sh
@@ -2,6 +2,9 @@ export BASH_ROOT="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
 
 NVBIT_VERSION=1.5.5
 
+echo "Warning: nvbit is already included in this Git repository by default."
+exit 0 
+
 rm -rf $BASH_ROOT/nvbit_release
 wget https://github.com/NVlabs/NVBit/releases/download/$NVBIT_VERSION/nvbit-Linux-x86_64-$NVBIT_VERSION.tar.bz2
 tar -xf nvbit-Linux-x86_64-$NVBIT_VERSION.tar.bz2 -C $BASH_ROOT
