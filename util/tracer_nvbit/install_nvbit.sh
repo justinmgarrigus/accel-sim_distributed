36ac67597a04f4ef88ed6368905f2fdc59fe96ad
Justin Garrigus <60117564+justinmgarrigus@users.noreply.github.com>
2024-09-25 10:35:00 -0500

    Removed download for nvbit again.

--- util/tracer_nvbit/install_nvbit.sh
+++ util/tracer_nvbit/install_nvbit.sh
@@ -3,8 +3,8 @@ export BASH_ROOT="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
 # NVBIT_VERSION=1.5.5
 NVBIT_VERSION=1.5.3
 
-# echo "Warning: nvbit is already included in this Git repository by default."
-# exit 0 
+echo "Warning: nvbit is already included in this Git repository by default."
+exit 0 
 
 rm -rf $BASH_ROOT/nvbit_release
 wget https://github.com/NVlabs/NVBit/releases/download/$NVBIT_VERSION/nvbit-Linux-x86_64-$NVBIT_VERSION.tar.bz2
